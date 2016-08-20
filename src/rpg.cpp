7b71890587821a968936e376db11c829230113d6
AdamBignell <adambignell@gmail.com>
2016-08-20 16:20:32 -0700

    Added the plus to second argument of open

--- src/rpg.cpp
+++ src/rpg.cpp
@@ -4,12 +4,16 @@
  */
  
 #include <cstdio>
+#include "iostream"
 #include <cstring>
+
+using namespace std;
  
 int main (int argc, char* argv[])
 {
 	// Check for characters.csv file, create one if it doesn't exist
-	FILE* f = fopen("characters.csv", "w");
+
+	FILE* f = fopen("characters.csv", "w+");
 	if (f == NULL)
 	{
 		printf("DOESN'T EXIST :(");
