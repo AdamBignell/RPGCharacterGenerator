08838e92acfd174ad8c4486f6df7b367f161bb78
Alister Pineda <alisterpineda@gmail.com>
2016-08-20 17:52:50 -0600

    added utilities.cpp

---
+++ src/utilities.cpp
@@ -0,0 +1,15 @@
+/* utilities.c
+
+All of these functions are subtle utilities that will ease writing.
+
+*/
+
+#include "utilities.h"
+
+/*
+ * GetInt - prompt user for an integer
+ */
+int GetInt(void)
+{
+	return 0;
+}
