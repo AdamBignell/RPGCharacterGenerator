ce0b921a66489eece6d42aebd5b0f790d8a2e8ca
Alister Pineda <alisterpineda@gmail.com>
2016-08-20 17:46:28 -0600

    added partially complete main menu system

--- src/rpg.cpp
+++ src/rpg.cpp
@@ -12,10 +12,27 @@ using namespace std;
 int main (int argc, char* argv[])
 {
 	// Check for characters.csv file, create one if it doesn't exist
-	FILE* f = fopen("characters.csv", "w+");
-	if (f == NULL)
+	FILE* cf = fopen("characters.csv", "w+");
+	if (cf == NULL)
 	{
 		printf("DOESN'T EXIST :(\n");
+	}
+	
+	FILE* qf= fopen("questions.csv", "r")
+	if (qf == NULL)
+	{
+		printf("DOESN'T EXIST :(\n");
+	}
+	
+	int sel;
+	
+	while(1)
+	{
+		printf("Select an option:\n");
+		printf("1	-	Generate Character");
+		printf("0	-	Exit");
+		printf("\n\nSelect: ");
+		
 	}
 	
 	fclose(f);
