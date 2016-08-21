5f3b258991ac75c0749ef19f2f7188108f0385d6
AdamBignell <adambignell@gmail.com>
2016-08-20 18:31:28 -0700

    Tried to implement the CSV parser for questions

--- src/utilities.h
+++ src/utilities.h
@@ -9,7 +9,39 @@ All of these functions are subtle utilities that will ease writing.
 using namespace std;
 #include "questions.h"
 
-bool CSVtoQuestions(string fileName, question* questions){
-	ifstream csv( fileName );
+bool CSVtoQuestions(question* questions){
+	string line;
+	ifstream csv("questions.csv");
+	int flag = 0;
+	int qNum = 0;
+	if (csv){
+		while(getline( csv, line, ',' )){
+			if (flag = 0){
+				questions[qNum].question = line;
+			}
+			if (flag = 1){
+				questions[qNum].optionA = line;
+			}
+			if (flag = 2){
+				questions[qNum].aConseq = line;
+			}
+			if (flag = 3){
+				questions[qNum].optionB = line;
+			}
+			if (flag = 4){
+				questions[qNum].bConseq = line;
+			}
+			if (flag = 5){
+				questions[qNum].optionC = line;
+			}
+			if (flag = 6){
+				questions[qNum].cConseq = line;
+				qNum++;
+			}
+			flag = (flag + 1) % 7;
 
+		}
+	return true;
+	}
+	return false;
 }
