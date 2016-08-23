8ff103e7168868d4eadd0f7064b98372f4c45eab
AdamBignell <adambignell@gmail.com>
2016-08-22 17:52:20 -0700

    Wrapped the questions function

--- src/rpg.cpp
+++ src/rpg.cpp
@@ -83,52 +83,7 @@ character* generate_character(question* questions)
 
 	newChar->setName(name);
 
-	for (int i = 0; i < NUM_Q; i++){
-		// This loop "asks" all the questions loaded from the csv
-		// Note that it requires proper setting of NUM_Q lest the program seg fault
-		string choice;
-		cout << "\n" << questions[i].question << endl;
-		cout << "\t A." << questions[i].optionA << endl;
-		cout << "\t B." << questions[i].optionB << endl;
-		cout << "\t C." << questions[i].optionC << endl;
-		cout <<  "\nPlease enter A, B, or C:" << endl;
-		cin >> choice;
-
-		string statChange;
-		if (choice == "A"){
-			statChange = questions[i].aConseq;
-		}
-		if (choice == "B"){
-			statChange = questions[i].bConseq;
-		}
-		if (choice == "C"){
-			statChange = questions[i].cConseq;
-		}
-
-
-		// Note that the ' == 0' is strictly necessary
-		// As well, be very prudent with whitespace in the CSV 'attribute' fields
-		if (strcmp(statChange.c_str(), "cStr") == 0){
-			newChar->incrcStr(1);
-		}
-		if (strcmp(statChange.c_str(), "cInt") == 0){
-			newChar->incrcInt(1);
-		}
-		if (strcmp(statChange.c_str(), "cEnd") == 0){
-			newChar->incrcEnd(1);
-		}
-		if (strcmp(statChange.c_str(), "cChar") == 0){
-			cout << "why?" << endl;
-			newChar->incrcChar(1);
-		}
-		if (strcmp(statChange.c_str(), "cWill") == 0){
-			newChar->incrcWill(1);
-		}
-		if (strcmp(statChange.c_str(), "cAgil") == 0){
-			newChar->incrcAgil(1);
-		}
-	}
-
+	askQuestions(questions, newChar);
 	// Need something that interprets profession. To be discussed with Alister
 
 	return newChar; // Return a now-complete instance of the player
